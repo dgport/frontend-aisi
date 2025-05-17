c940e3b537eb79930c89ec0e47237dd08d3c55ac
Lado <lado.asambadze1@gmail.com>
2025-05-18 01:04:32 +0400

    build

--- .next/server/chunks/ssr/src_messages_ka_main_json_1f70b87c._.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-"[project]/src/messages/ka/main.json (json)": ((__turbopack_context__) => {
-
-var { g: global, __dirname } = __turbopack_context__;
-{
-__turbopack_context__.v(JSON.parse("{\"hello\":\"გამარჯობა\"}"));}}),
-
-};
